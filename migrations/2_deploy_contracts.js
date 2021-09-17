1f66dfddcc919e59dc30324487e70b76cb3a3f4a
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-09-17 10:47:37 +0530

    fix: change weth address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const WETH = artifacts.require("WETH");
 const Blender = artifacts.require("ApeBlenderImpl");
 module.exports = async function (deployer) {
-  await deployer.deploy(WETH); // deploy WETH
-  const weth = await WETH.deployed();
+  // await deployer.deploy(WETH); // deploy WETH
+  const weth = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
   const apeFeeBps = 50;
   const feeTreasury = "0x40d5250D1ce81fdD1F0E0FB4F471E57AA0c1FaD3";
   const exchangeRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; //uniswapv2 router in Ropsten
@@ -13,7 +13,7 @@ module.exports = async function (deployer) {
     apeFeeBps,
     feeTreasury,
     exchangeRouter,
-    weth.address,
+    weth,
     exchangeSwapFeeNumerator,
     exchangeSwapFeeDenominator
   ); // deploy of contract
